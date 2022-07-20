098a22420827eeded4c89d681f51ff16f98bf07e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-20 17:54:08 -0500

    Make button functional

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const nameInput = document.getElementById('name-input');
 const updateButton = document.getElementById('update-button');
 
 console.log(nameDiv, nameInput, updateButton);
+
+// add an event listener to the button - "listen for the click"
+updateButton.addEventListener('click', () => {
+    // get the value of the input
+    const inputText = nameInput.value;
+    // replace the name div text content with the input
+    nameDiv.textContent = inputText;
+});
